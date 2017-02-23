01e85ca1bf8020bb0ad839f46df758f0d61dfeda
emmanuelmugogo <mugoclan87@gmail.com>
2017-02-23 12:16:20 -0500

    factorizer commit

---
+++ Factorizer/src/main/java/Factorizer.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Factorizer {
+
+	public static void main(String[] args) {
+
+		int num, sum = 0;
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Please enter the number to factorize.");
+		num = scanner.nextInt();
+
+		for (int i = 1; i < num; i++) {
+			if (num % i == 0) {
+				System.out.println(i);
+				sum += i;
+			}
+
+		}
+//		System.out.println("the total of factors is " + sum);
+
+		if (sum == num) {
+			System.out.println("this is a perfect number");
+		} else if (sum != num) {
+			System.out.println(num + " is not a perfect number");
+		}
+
+		if (sum == 1) {
+			System.out.println(num + " is a prime number");
+		}
+		scanner.close();
+	}
+
+}
